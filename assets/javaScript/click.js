043cce3a7833cba47956df4189ef30b1df7fb31e
Kevin-alcaino <wthblck1@gmail.com>
2026-07-03 19:22:28 -0400

    se crea el border de la imagen con onclick

--- assets/javaScript/click.js
+++ assets/javaScript/click.js
@@ -1,15 +1,1 @@
-const temperatura = Number(prompt("ingrese su temperatura"))
-if (isNaN(temperatura)) {
-    alert("debe ingresar numeros")
-}
-else if (temperatura < 0) {
-    alert("hace mucho frio")
-} else if (temperatura < 10) {
-    alert("hace frio")
-} else if (temperatura > 11 && temperatura <=20) {
-    alert("temperatura agradable ")
-} else if (temperatura <=30) {
-    alert("hace calor")
- else {
-    alert("hace mucho calor")
- }
+const borde = 
